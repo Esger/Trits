15a08b558959687c1fc309fce27ae0d524a7f4be
Esger <esgerj@gmail.com>
2024-12-30 14:46:59 +0100

    moved treats to tile

--- src/resources/elements/board.js
+++ src/resources/elements/board.js
@@ -9,14 +9,10 @@ export class Board {
 
     constructor(eventAggregator) {
         this._eventAggregator = eventAggregator;
-        this.textTranforms = ['capitalize', 'uppercase', 'lowercase'];
-        this.colors = ['crimson', 'olive', 'navy'];
-        this.backgrounds = ['lightgoldenrodyellow', 'wheat', 'lightblue'];
-        this.tiles = [];
         this.tileCount = this.WIDTH * this.HEIGHT;
-        for (let i = 0; i < this.tileCount; i++) {
-            this.tiles.push(this.newTile());
-        }
+        this.tiles = [];
+        for (let i = 0; i < this.tileCount; i++)
+            this.tiles.push({ id: i });
     }
 
     attached() {
@@ -27,43 +23,12 @@ export class Board {
         this._clickSubscription.dispose();
     }
 
-    _randomProperty(properties) {
-        return properties[Math.floor(Math.random() * properties.length)]
-    }
-
-    newTile() {
-        const tile = {
-            color: this._randomProperty(this.colors),
-            background: this._randomProperty(this.backgrounds),
-            textTransform: this._randomProperty(this.textTranforms),
-        };
-        return tile;
-    }
-
-    _renewTiles(tiles) {
-        const baseTimeOut = 500;
-        let timeOut = 500;
-        for (let tile of tiles) {
-            setTimeout(_ => {
-                tile.visible = false;
-                setTimeout(_ => {
-                    tile.color = this._randomProperty(this.colors);
-                    tile.background = this._randomProperty(this.backgrounds);
-                    tile.textTransform = this._randomProperty(this.textTranforms);
-                    tile.marked = false;
-                    tile.visible = true;
-                });
-            }, timeOut);
-            timeOut += baseTimeOut;
-        }
-    }
-
     _checkWin() {
         const markedTiles = this.tiles.filter(tile => tile.marked);
         if (markedTiles.length === 3) {
             if (markedTiles.every(tile => tile.color === markedTiles[0].color)) {
                 alert('You won!');
-                this._renewTiles(markedTiles);
+                this._eventAggregator.publish('renew-tiles', markedTiles);
             } else {
                 markedTiles.forEach(tile => tile.marked = false);
             }
--- src/resources/elements/tile.js
+++ src/resources/elements/tile.js
@@ -3,18 +3,57 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 
 @inject(EventAggregator)
 export class Tile {
-    @bindable value;
+    @bindable tileObj;
 
     constructor(eventAggregator) {
         this._eventAggregator = eventAggregator;
+        this.treats = {
+            textTranform: ['capitalize', 'uppercase', 'lowercase'],
+            color: ['crimson', 'olive', 'navy'],
+            background: ['lightgoldenrodyellow', 'wheat', 'lightblue']
+        }
     }
 
-    bind() {
-        this.value.marked = false;
+    attached() {
+        this._randomizeTreats();
+        this.tileObj.marked = false;
+        this.tileObj.visible = true;
+        this._renewSubscription = this._eventAggregator.subscribe('renew-tiles', tiles => {
+            const renewTile = tiles.some(tile => tile.id === this.tileObj.id);
+            if (renewTile)
+                this._renew()
+        });
+    }
+
+    detached() {
+        this._renewSubscription.dispose();
     }
 
     clicked() {
-        this.value.marked = !this.value.marked;
+        this.tileObj.marked = !this.tileObj.marked;
         this._eventAggregator.publish('tile-clicked');
+    }
+
+    _randomizeTreats() {
+        const treats = Object.keys(this.treats);
+        for (let treat of treats) {
+            this.tileObj[treat] = this._randomProperty(this.treats[treat]);
+        }
+    }
+
+    _randomProperty(properties) {
+        return properties[Math.floor(Math.random() * properties.length)]
+    }
+
+    _renew() {
+        let timeOut = 500;
+        setTimeout(_ => {
+            this.tileObj.visible = false;
+            setTimeout(_ => {
+                this._randomizeTreats();
+                this.tileObj.marked = false;
+                this.tileObj.visible = true;
+            }, timeOut);
+        }, timeOut);
     }
 }
