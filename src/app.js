dc2dbb50d74758b9fae75b86501d6418126457b0
Esger <esgerj@gmail.com>
2025-01-02 02:13:42 +0100

    show hint and score -2

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class App {
+    constructor(eventAggregator) {
+        this._eventAggregator = eventAggregator;
+    }
+
+    hint() {
+        this._eventAggregator.publish('hint');
+    }
+
 }
