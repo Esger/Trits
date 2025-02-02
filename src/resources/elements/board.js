751ae87ce96d4b2a0758b8896232de3f337f1eac
Esger <esgerj@gmail.com>
2025-02-02 02:30:56 +0100

    lijnraster op deck niet drawn

--- src/resources/elements/board.js
+++ src/resources/elements/board.js
@@ -64,7 +64,7 @@ export class Board {
 
     highLightTiles() {
         this._lastCombinationIndex = this._lastCombinationIndex !== undefined ? (this._lastCombinationIndex + 1) % this.allCorrectCombinations.length : 0;
-        this._highlightTiles(this.allCorrectCombinations[this._lastCombinationIndex]);
+        this._lastCombinationIndex !== undefined && this._highlightTiles(this.allCorrectCombinations[this._lastCombinationIndex]);
     }
 
     _getRandomIndex() {
